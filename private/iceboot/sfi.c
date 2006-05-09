3bde34f5cecfb99549f0b7815e628a4731f6fabb
Arthur Jones <arthur@lbl.gov>
2006-05-09 12:42:26 -0500

    release 412

--- private/iceboot/sfi.c
+++ private/iceboot/sfi.c
@@ -85,9 +85,9 @@
  * \section notes Notes
  *   requires vt100 terminal set to 115200,N,8,1 hardware flow control...
  *
- * $Revision: 1.1.1.10 $
+ * $Revision: 1.1.1.11 $
  * $Author: arthur $
- * $Date: 2006-05-01 23:10:56 $
+ * $Date: 2006-05-09 22:42:23 $
  */
 #include <stdio.h>
 #include <string.h>
